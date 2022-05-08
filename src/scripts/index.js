4b315589d5a642e974400a4b63d4440698ff6b99
Krvknv <cristina.krivokoneva@yandex.ru>
2022-05-08 17:13:37 +0300

    init: start task

---
+++ src/scripts/index.js
@@ -0,0 +1,5 @@
+import "../assets/styles/style.css";
+import "../assets/styles/style.scss";
+
+const u = 42;
+console.log(u);
---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ESLintPlugin = require("eslint-webpack-plugin");
+
+module.exports = {
+  entry: "./src/scripts/index.js",
+  output: {
+    filename: "[hash][name].js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  mode: "development",
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Virtual Keyboard",
+      filename: "index.html",
+      template: "src/index.html",
+    }),
+    new ESLintPlugin(),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+    ],
+  },
+  devServer: {
+    static: "./dist",
+    port: 3000,
+  },
+};
